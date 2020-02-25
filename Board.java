fab24596c34c36b85b99280e623baf45791b1745
Raymond Jolly <raymond.jolly@gmail.com>
2020-02-24 21:34:15 -0500

    implemented changes to the setGamePiece method

--- Board.java
+++ Board.java
@@ -8,55 +8,78 @@ public class Board {
 
     public Board(int rows, int cols) {
         this.board = new Integer[rows][cols];
-        for(Integer[] row: board){
+        for (Integer[] row : board) {
             Arrays.fill(row, 0);
         }
     }
 
-    public Integer getPiece(int row, int col){
+    public Integer getGamePiece(int row, int col) {
         return this.board[row][col];
     }
 
-    public void setPiece(int row, int col, int value){
-        this.board[row][col] = value;
+    public int getFreeSpaceLeftInColumn(int col) {
+        int count = 0;
+        for (int i = 0; i < getBoardHeight(); i++) {
+            if (board[i][col].equals(0)) {
+                count += 1;
+            }
+        }
+        return count;
     }
 
-    public int getBoardWidth(){
+    public boolean isColumnPlayable(int col) {
+        if (getFreeSpaceLeftInColumn(col) != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setGamePiece(int col, int value) {
+        int count = 0;
+        if (getFreeSpaceLeftInColumn(col)>=0) {
+            for (int i = 0; i < getBoardHeight(); i++) {
+                if (board[i][col].equals(0)) {
+                    count += 1;
+                }
+                board[getBoardHeight() - count][col] = value;
+            }
+        }
+    }
+
+    public int getBoardWidth() {
         return this.board.length;
     }
 
-    public int getBoardHeight(){
+    public int getBoardHeight() {
         return this.board[0].length;
     }
 
-    public Integer[][] getBoardValues(){
+    public Integer[][] getBoardValues() {
         return this.board;
     }
 
-    public void printBoardValues(){
-        //print column headers
-        System.out.print("     ");
-        for(int xLabel = 0; xLabel<getBoardWidth(); xLabel++){
-            System.out.println(String.format("%2s", xLabel));
+    public void printBoard(){
+        for(int xLabel=0; xLabel< getBoardWidth(); xLabel++){
+            System.out.print(String.format("%4s", xLabel +1));
         }
         System.out.println();
         //print board
-        for(int row=0; row<getBoardHeight(); row++){
-            System.out.print(String.format("%2s", row+1));
+        for(int row =0; row<getBoardWidth(); row++){
+            System.out.print("| ");
             for(int col=0; col<getBoardWidth(); col++){
-                switch (getPiece(row, col)){
+                switch (getGamePiece(row, col)){
                     case 1:
-                        System.out.print("P1");
+                        System.out.print("P1|");
                         break;
                     case 2:
-                        System.out.print("P2");
+                        System.out.print("P2|");
                         break;
                     case 3:
-                        System.out.print("P3");
+                        System.out.print("P3|");
                         break;
                     default:
-                        System.out.println(".");
-                        break;
+                        System.out.print(". |");
                 }
                 System.out.print(" ");
             }
--- Main.java
+++ Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Board board = new Board(4, 4);
+        board.printBoard();
+        board.setGamePiece(0, 1);
+        System.out.println(board.getFreeSpaceLeftInColumn(0));
+        board.printBoard();
     }
 }
